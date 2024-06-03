d6a19338ad311111597bd62248f27d6cf140d285
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-03 18:07:15 +0200

    Update Temples.java

--- SAE/src/main/java/modele/Temples.java
+++ SAE/src/main/java/modele/Temples.java
@@ -1,23 +1,40 @@
 package modele;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Random;
+public class Temple implements Comparable <Temple> {
+    private Position chPosition;
+    private int chCouleur, chCouleurCristal;
 
-public class Temples implements ConstanteCanvas {
-    private Position chPosTemple;
-    private int chCouleurTemple;
-    private int chCouleurCristal;
-
-    public Temples(Position parPosTemple, int parCouleurTemple, int parCouleurCristal) {
-        chPosTemple = parPosTemple;
-        chCouleurTemple = parCouleurTemple;
-        chCouleurCristal = parCouleurCristal;
+    public Temple(Position parPos, int parCoul, int parCoulCristal) {
+        chPosition = new Position(parPos.getPosX(), parPos.getPosY());
+        chCouleur = parCoul;
+        chCouleurCristal = parCoulCristal;
     }
 
-    public Position getPos() { return chPosTemple; }
-    public String getCouleurTemple() { return LIST_COULEURS[chCouleurTemple];}
+    public String toString() {
+        return chPosition.toString() + ", " + chCouleur + ", " + chCouleurCristal;
+    }
+
+    public Position getPosition() {
+        return chPosition;
+    }
+
+    public int getCouleur() {
+        return chCouleur;
+    }
+
+    public int getCouleurCristal() {
+        return chCouleurCristal;
+    }
+
+    public void setCouleurCristal(int parCoul) {
+        chCouleurCristal = parCoul;
+    }
+
+    public boolean estAligne() {
+        return chCouleurCristal == chCouleur;
+    }
+
+    public int compareTo(Temple parTemple) {
+        return chCouleur - parTemple.chCouleur;
+    }
 }
-
-
