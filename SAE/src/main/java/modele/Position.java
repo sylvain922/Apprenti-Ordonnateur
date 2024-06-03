3e6157a018d9e40a26b2d0cccf2472d441cfa777
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-03 18:06:38 +0200

    Update Position.java

--- SAE/src/main/java/modele/Position.java
+++ SAE/src/main/java/modele/Position.java
@@ -1,77 +1,68 @@
 package modele;
 
 public class Position {
-    private static int nombreDePas = 0;
-    private int abscisse;
-    private int ordonnee;
+    private int chPosX, chPosY;
+    private int chNbDepl = 0;
 
-    public Position(int abscisse, int ordonnee){
-        this.abscisse = abscisse;
-        this.ordonnee = ordonnee;
+    public Position(int parX, int parY) {
+        chPosX = parX;
+        chPosY = parY;
     }
 
-    /**
-     * la méthode deplacementUneCase déplace la position this d'une case
-     * pour la rapprocher de celle du paramètre parPosition
-     * elle incrémente le champ static nombreDePas
-     * @param parPosition : la position vers laquelle this se rapproche**/
-    public void deplacementUneCase(Position parPosition){
-        nombreDePas++;
-        if (this.abscisse > parPosition.abscisse){
-            this.abscisse -=1;
-            return;
-        }
-        else if (this.abscisse < parPosition.abscisse){
-            this.abscisse +=1;
-            return;
-        }
-        if (this.ordonnee > parPosition.ordonnee){
-            this.ordonnee -=1;
-            return;
-        }
-        else if (this.ordonnee < parPosition.ordonnee){
-            this.ordonnee +=1;
-            return;
-        }
+    public Position(Position parPos) {
+        chPosX = parPos.chPosX;
+        chPosY = parPos.chPosY;
     }
 
-    /**
-     * Méthode pour comparer deux positions et déterminer si elles sont égales.
-     * @param objet : l'objet à comparer
-     * @return true si les positions sont égales, false sinon**/
-    public boolean equals(Object objet) {
-        if (this == objet){
-            return true;
-        }
-        if (objet == null || getClass() != objet.getClass()){
-            return false;
-        }
-        Position position = (Position) objet;
-        return abscisse == position.abscisse &&
-                ordonnee == position.ordonnee;
-    }
-
-    // Accesseurs nécessaires
-
-    public int getAbscisse() {
-        return abscisse;
-    }
-
-    public int getOrdonnee() {
-        return ordonnee;
-    }
-
-    public static int getNombreDePas() {
-        return nombreDePas;
-    }
-
-    /**
-     * Méthode pour afficher la position sous forme de chaîne de caractères.
-     * @return la position sous forme de chaîne de caractères**/
     public String toString() {
-        return "Position{" +
-                "abscisse=" + abscisse +
-                ", ordonnee=" + ordonnee +
-                '}';
+        return "[" + chPosX + ", " + chPosY + "]";
     }
-}
+
+    public int getPosX() {
+        return chPosX;
+    }
+
+    public int getPosY() {
+        return chPosY;
+    }
+
+    public int gerNbDepl() {
+        return chNbDepl;
+    }
+
+    public void setNbDepl(int parNb) {
+        chNbDepl = parNb;
+    }
+
+    public void deplUneCase(Position parPos) {
+        chNbDepl++;
+        if (chPosX < parPos.chPosX) {
+            chPosX++;
+            return;
+        }
+        if (chPosX > parPos.chPosX) {
+            chPosX--;
+            return;
+        }
+        if (chPosY < parPos.chPosY) {
+            chPosY++;
+            return;
+        }
+        if (chPosY > parPos.chPosY) {
+            chPosY--;
+            return;
+        }
+    }
+
+    public void setPosition(Position parPos) {
+        chPosX = parPos.chPosX;
+        chPosY = parPos.chPosY;
+    }
+
+    public int distance(Position parPos) {
+        return Math.abs(chPosX - parPos.chPosX) + Math.abs(chPosY - parPos.chPosY);
+    }
+
+    public boolean estMemePosition(Position parPos) {
+        return (parPos.chPosX == chPosX && parPos.chPosY == chPosY);
+    }
