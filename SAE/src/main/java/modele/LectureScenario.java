5e34c99f24166ce2e8ce7df1ae38e411707831db
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-03 18:05:24 +0200

    Update LectureScenario.java

--- SAE/src/main/java/modele/LectureScenario.java
+++ SAE/src/main/java/modele/LectureScenario.java
@@ -16,7 +16,7 @@ public class LectureScenario implements Constantes {
 
     public LectureScenario(String parNomFich) {
         try {
-            Scanner sc = new Scanner(new File("scenarios" + File.separator + parNomFich)); //.useDelimiter(",\\s*");
+            Scanner sc = new Scanner(new File("scenarios" + File.separator + parNomFich));
             while (sc.hasNext()) {
                 int x = sc.nextInt() + NB_L/2;
                 int y = sc.nextInt() + NB_H/2;
@@ -27,7 +27,6 @@ public class LectureScenario implements Constantes {
             }
             chListeHeuristique = heuristique();
             chListeTriSelection = triSelection();
-            //System.out.println("nb pas heuris : " + calculerNbPas(chListeHeuristique) + ", tri : " + calculerNbPas(chListeTriSelection));
 
         } catch (FileNotFoundException ex){
             System.err.println("FileNotFoundException");
@@ -37,3 +36,120 @@ public class LectureScenario implements Constantes {
             System.exit(-1);
         }
     }
+    
+    public String toString() {
+        String str = "";
+        for (Temple t : chTemples) {
+            str += t.toString();
+        }
+        return str;
+    }
+
+    public ArrayList <Temple> getTemples() {
+        return chTemples;
+    }
+
+    public Apprenti getApprenti() {
+        return chApprenti;
+    }
+    
+    public Temple templeLePlusPresApprenti() {
+        int distanceMin = 100;
+        Temple templeMin = chTemples.get(0);
+        for (Temple t : chTemples) {
+            if (chApprenti.getPosition().distance(t.getPosition()) < distanceMin) {
+                distanceMin = chApprenti.getPosition().distance(t.getPosition());
+                templeMin = t;
+            }
+        }
+        return templeMin;
+    }
+
+    public Temple getTemple(int couleurTemple) {
+        for (Temple t : chTemples) {
+            if (t.getCouleur() == couleurTemple)
+                return t;
+        }
+        return null;
+    }
+
+    public Temple getTemple(Position pos) {
+        for (Temple t : chTemples) {
+            if (pos.estMemePosition(t.getPosition()))
+                return t;
+        }
+        return null;
+    }
+
+    private int indexMinimum(int []tab, int deb, int fin) {
+        int iMin = deb;
+        for (int i = deb+1; i <= fin; i++) {
+            if (tab[i] < tab[iMin])
+                iMin = i;
+        }
+        return iMin;
+    }
+
+    public ArrayList<Position> triSelection() {
+        ArrayList<Position> listePosTemple = new ArrayList<>();
+        Temple tabTemples [] = new Temple[chTemples.size()];
+        int tabCoulCristal[] = new int[chTemples.size()];
+        int i = 0;
+        for (Temple t : chTemples) {
+            tabTemples[i++] = t;
+        }
+        Arrays.sort(tabTemples);
+        for (i = 0; i < chTemples.size(); i++)
+            tabCoulCristal[i] = tabTemples[i].getCouleurCristal();
+
+        for (i = 0; i < chTemples.size(); i++) {
+            int iMin = indexMinimum(tabCoulCristal, i, chTemples.size()-1);
+            if (iMin == i)
+                continue;
+
+            int tmp = tabCoulCristal[i];
+            tabCoulCristal[i] = tabCoulCristal[iMin];
+            tabCoulCristal[iMin] = tmp;
+
+            listePosTemple.add(tabTemples[i].getPosition());
+            listePosTemple.add(tabTemples[iMin].getPosition());
+            listePosTemple.add(tabTemples[i].getPosition());
+        }
+        return  listePosTemple;
+    }
+
+    public ArrayList <Position> heuristique() {
+        ArrayList <Position> listePosTemple = new ArrayList<>();
+        Temple templeProchain = templeLePlusPresApprenti();
+        for (int i = 0; i < (chTemples.size()+1); i++) {
+            listePosTemple.add(templeProchain.getPosition());
+            int coulCristal = templeProchain.getCouleurCristal();
+            templeProchain = getTemple(coulCristal);
+        }
+        return listePosTemple;
+    }
+
+    public int calculerNbPas(ArrayList <Position> listePos) {
+        int pas = chApprenti.getPosition().distance(listePos.get(0));
+        for (int i = 0; i < (listePos.size()-1); i++) {
+            pas += listePos.get(i).distance(listePos.get(i+1));
+        }
+        return pas;
+    }
+
+    public int calculerNbPasTriSelection() {
+        return calculerNbPas(chListeTriSelection);
+    }
+
+    public int calculerNbPasHeuristique() {
+        return calculerNbPas(chListeHeuristique);
+    }
+
+    public ArrayList <Position> getListeHeuristique() {
+        return chListeHeuristique;
+    }
+
+    public ArrayList <Position> getChListeTriSelection() {
+        return chListeTriSelection;
+    }
+}
