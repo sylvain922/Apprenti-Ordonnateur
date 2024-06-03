4f4a3d122a3f1392b0e8a42369c5c45b20289e0d
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-04 01:19:27 +0200

    Update Constantes.java

--- SAE/src/main/java/vue/Constantes.java
+++ SAE/src/main/java/vue/Constantes.java
@@ -2,31 +2,24 @@ package vue;
 
 import javafx.scene.paint.Color;
 import modele.Position;
-
 import java.io.File;
 
 public interface Constantes {
-    public final int CARRE = 32, CARRE_TEMPLE = CARRE - 2;
-    public final int DIAM_CRISTAL = CARRE_TEMPLE - 12;
-    public final int NB_L = 31, NB_H = 25;
-    public final int L_CANVAS = NB_L * CARRE, H_CANVAS = NB_H * CARRE;
-    public final int L_FENETRE = L_CANVAS + 400, H_FENETRE = H_CANVAS;
+    public final int CARRE = 32;
+    public final int LARGEUR = 31, HAUTEUR = 25;  //16, 13
+    public final int LARGEUR_CANVAS = LARGEUR * CARRE, HAUTEUR_CANVAS = HAUTEUR * CARRE;
+    public final int LARGEUR_FENETRE = LARGEUR_CANVAS + 400, HAUTEUR_FENETRE = HAUTEUR_CANVAS;
 
-    public final int L_CRISTAL = 10, H_CRISTAL = 25;
+    public final Color COULEUR_FOND = Color.web("#353535");    //Couleur 0x202020 en héxadécimal
 
-    public final Color COULEUR_FOND = Color.web("#353535");
+    public final String [] LIST_COULEURS = {"bleu","jaune","orange","rose","rouge","vert","violet","marron","gris"};
 
-    public final Color COULEURS_TEMPLES[] = {Color.WHITE, Color.CYAN, Color.CHOCOLATE, Color.CORNFLOWERBLUE, Color.BROWN, Color.YELLOWGREEN, Color.VIOLET, Color.YELLOW, Color.DARKCYAN, Color.CRIMSON};
+    public final String IMAGE_APPRENTI = "img" + File.separator + "aventurier.PNG";
 
-    public final String ICONE_APPRENTI = "icones" + File.separator + "aventurier.PNG";
+    public final Position POSITION_APPRENTI = new Position(LARGEUR/2, HAUTEUR/2);
 
-    public final Position POS_APPRENTI = new Position(NB_L/2, NB_H/2);
+    public final int MANUEL = 0, HEURISTIQUE = 1, SELECTION = 2;
 
-    public final int MANUEL = 0, HEURISTIQUE = 1, TRI = 2;
-
-    public final String TITRE_BOUTON_HEURIS = "Heuristique", TITRE_BOUTON_TRI = "Tri Selection", TITRE_BOUTON_RESET = "Reset";
-
-    public String NOM_FICH = "scenario2.txt";
-
-    public final int H_GRIDPANE = 200;
+    public final String TITRE_BOUTON_HEURISTIQUE = "Heuristique", TITRE_BOUTON_SELECTION = "Tri Selection", TITRE_BOUTON_RESET = "Reset";
 }
+
