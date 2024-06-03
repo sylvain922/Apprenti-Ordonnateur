bc906e8ed06a1f89760bc87e3e7d5b20cbfc132d
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-03 17:40:57 +0200

    Update ApprentiOrdonnateurApplication.java

--- SAE/src/main/java/vue/ApprentiOrdonnateurApplication.java
+++ SAE/src/main/java/vue/ApprentiOrdonnateurApplication.java
@@ -2,24 +2,32 @@ package vue;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 
-public class ApprentiOrdonnateurApplication extends Application {
-    public void start(Stage stage) {
-        HboxRoot root = new HboxRoot();
-        Scene scene = new Scene(root, 1500, 950);
-        /*File [] fichiersCss = new File("css").listFiles();
-        for (File fichier : fichiersCss) {
-            //scene.getStylesheets().add(fichier.toURI().toString());
-        }*/
+public class ApprentiOrdonnateur extends Application implements Constantes {
+    public void start(Stage stage)  {
+        HBoxRoot root = new HBoxRoot();
+        //Scene scene  = new Scene(root);
+        MenuScene menuScene = new MenuScene(stage);
+        Scene scene = new Scene(menuScene, L_FENETRE, H_FENETRE);
+        //MenuScene menuScene = new MenuScene(scene);
+        File file = new File("icones/fond.png");
+        String cheminImage = file.toURI().toString();
+        menuScene.setStyle(
+                "-fx-background-image: url('" + cheminImage + "');"+
+                        "-fx-background-size: cover;"
+        );
         stage.setScene(scene);
-        stage.setTitle("Apprenti Ordonnateur");
+        stage.setTitle("SAE L'Apprenti Ordonnateur");
         stage.show();
+
+        File css = new File("css" + File.separator + "sae.css");
+        scene.getStylesheets().add(css.toURI().toString());
     }
-    public static void main (String [] args) {
-        Application.launch(args);
+
+    public static void main(String [] arg) {
+        Application.launch(arg);
     }
 }
