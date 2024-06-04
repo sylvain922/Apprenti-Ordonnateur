1c215eb6eaa980cf2208827281c742536e1eb442
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-04 21:36:24 +0200

    ajout de la documentation

--- SAE/src/main/java/vue/CanvasCartes.java
+++ SAE/src/main/java/vue/CanvasCartes.java
@@ -1,31 +1,34 @@
 package vue;
+
 import javafx.application.Platform;
 import modele.*;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
+/**
+ * La classe CanvasCartes représente la carte du jeu affichée dans l'interface utilisateur.
+ * Elle étend la classe Canvas de JavaFX et gère l'affichage des éléments graphiques du jeu.
+ */
 public class CanvasCartes extends Canvas implements Constantes{
-    private VBoxInformations chVBoxInformations;
-    private GraphicsContext hGC;
+    private VBoxInformations chVBoxInfo;
+    private static GraphicsContext hGC;
     private Image chImageApprenti;
-    private Scenario chScenario;
+    private LectureScenario chScenario;
     private Apprenti chApprenti ;
+    private TreeMap<Integer, ArrayList<Image>> templeImagesMap = new TreeMap<>();
+    private TreeMap<Integer, ArrayList<Image>> cristalImageMap = new TreeMap<>();
 
+    /**
+     * Constructeur de la classe CanvasCartes.
+     */
     public CanvasCartes() {
-        super(L_CANVAS, H_CANVAS);
-        //chApprenti = HBoxRoot.getApprenti();
-        chVBoxInformations = HBoxRoot.getVBoxInfo();
-        //chNomFich = nomFich;
+        super(LARGEUR_CANVAS, HAUTEUR_CANVAS);
+        chVBoxInfo = HBoxRoot.getVBoxInfo();
         hGC = getGraphicsContext2D();
         affGrille();
 
@@ -33,11 +36,11 @@ public class CanvasCartes extends Canvas implements Constantes{
 
         setOnMouseClicked(event -> {
             Position positionCliquee = new Position((int) event.getX() / CARRE, (int) event.getY() / CARRE);
-            if (chScenario.estAligne() == false) {
+            if (!chScenario.estAligne()) {
                 Position pos = new Position(chApprenti.getPosition());
                 ArrayList<Position> liste = new ArrayList<>();
                 if (chScenario.getTemple(positionCliquee) == null) {
-                    chVBoxInformations.setInfo("Cliquez un temple SVP !");
+                    chVBoxInfo.setInfo("Cliquez un temple SVP !");
                     return;
                 }
                 liste.add(positionCliquee);
@@ -46,67 +49,75 @@ public class CanvasCartes extends Canvas implements Constantes{
         });
     }
 
+    /**
+     * Exécute l'action basée sur l'heuristique pour déplacer l'apprenti.
+     */
     public void actionHeuristique() {
-        if (chScenario.estAligne() == true)
+        if (chScenario.estAligne())
             return;
-        //ArrayList <Position> liste = chScenario.heuristique();
         deplApprenti(HBoxRoot.getScenario().getListeHeuristique(), HEURISTIQUE);
     }
 
-    public void actionTri() {
-        if (chScenario.estAligne() == true)
+    /**
+     * Exécute l'action basée sur le tri par sélection pour déplacer l'apprenti.
+     */
+    public void actionSelection() {
+        if (chScenario.estAligne())
             return;
-        //ArrayList <Position> liste = chScenario.triSelection();
-        deplApprenti(HBoxRoot.getScenario().getChListeTriSelection(), TRI);
+        deplApprenti(HBoxRoot.getScenario().getChListeTriSelection(), SELECTION);
     }
 
+    /**
+     * Affiche la grille sur le canvas.
+     */
     public void affGrille() {
-        hGC.setStroke(Color.LIGHTGRAY);      // grille
+        hGC.setStroke(Color.LIGHTGRAY);
         hGC.setLineWidth(1);
 
-        for (int y = 0; y < H_CANVAS; y += CARRE)
-            hGC.strokeLine(0, y, L_CANVAS, y);
-        for (int x = 0; x <= L_CANVAS; x += CARRE)
-            hGC.strokeLine(x, 0, x, H_CANVAS);
+        for (int y = 0; y < HAUTEUR_CANVAS; y += CARRE)
+            hGC.strokeLine(0, y, LARGEUR_CANVAS, y);
+        for (int x = 0; x <= LARGEUR_CANVAS; x += CARRE)
+            hGC.strokeLine(x, 0, x, HAUTEUR_CANVAS);
     }
 
-    /*public void setNomFich(String nom) {
-        chNomFich = nom;
-    }*/
-
-    public void resetPlateau(Scenario parScenario) {
-        if (chScenario != null) {                       // effacer tous temples
+    /**
+     * Réinitialise le plateau de jeu avec un nouveau scénario.
+     * Efface les temples du plateau, charge les images des temples et des cristaux,
+     * replace l'apprenti et les temples, et affiche les informations sur le côté droit.
+     *
+     * @param parScenario Le nouveau scénario à utiliser.
+     */
+    public void resetPlateau(LectureScenario parScenario) {
+        if (chScenario != null) {
             for (Temple temple : chScenario.getTemples())
                 effacerCase(temple.getPosition());
         }
 
-        try {                           // placer l'apprenti
-            chImageApprenti = new Image(new FileInputStream(ICONE_APPRENTI));
+        try {
+            chImageApprenti = new Image(new FileInputStream(IMAGE_APPRENTI));
         }
         catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        //if (chApprenti.getPosition().estMemePosition(POS_APPRENTI) == false)
-          //  effacerCase(chApprenti.getPosition());
         chScenario = HBoxRoot.getScenario();
         chApprenti = parScenario.getApprenti();
-
-        //chApprenti.setPosition(POS_APPRENTI);   // pour apprenti
-        //chApprenti.setCouleurCristal(0);
         placerApprenti();
         loadCristalImages();
         loadTempleImages();
-        chApprenti.getPosition().setNbDepl(0);
+        chApprenti.getPosition().setNbDeplacemenst(0);
 
-        chVBoxInformations.setNbDeplHeuris(0);         // pour les nb depl
-        chVBoxInformations.setNbDeplManuel(0);
-        chVBoxInformations.setNbDeplTri(0);
-        chVBoxInformations.setInfo("");
+        chVBoxInfo.setNbDeplacementsHeuristique(0);
+        chVBoxInfo.setNbDeplacementsJouer(0);
+        chVBoxInfo.setNbDeplacementsSelection(0);
+        chVBoxInfo.setInfo("");
 
         placerTemples(chScenario.getTemples(), templeImagesMap);
         placerCristaux();
     }
 
+    /**
+     * Place les cristaux sur les temples en fonction de leur couleur.
+     */
     public void placerCristaux() {
         for (Temple t : chScenario.getTemples()) {
             ArrayList<Image> images = cristalImageMap.get(t.getCouleurCristal()-1);
@@ -117,28 +128,40 @@ public class CanvasCartes extends Canvas implements Constantes{
         }
     }
 
+    /**
+     * Charge les images des temples à partir des ressources et les stocke dans la carte des temples.
+     */
     public void loadTempleImages() {
-        for (int couleur = 0; couleur < LIST_COULEURS.length; couleur++) {
+        for (int couleur = 0; couleur < COULEURS_TEMPLES.length; couleur++) {
             ArrayList<Image> images = new ArrayList<>();
-            for (int cristal = 0; cristal <= LIST_COULEURS.length; cristal++) {
-                String imagePath = "/temples/temple_" + LIST_COULEURS[couleur] + ".png";
+            for (int cristal = 0; cristal <= COULEURS_TEMPLES.length; cristal++) {
+                String imagePath = "/temples/temple_" + COULEURS_TEMPLES[couleur] + ".png";
                 images.add(new Image(getClass().getResourceAsStream(imagePath)));
             }
             templeImagesMap.put(couleur, images);
         }
     }
 
+    /**
+     * Charge les images des cristaux à partir des ressources et les stocke dans la carte des cristaux.
+     */
     public void loadCristalImages() {
-        for (int couleur = 0; couleur < LIST_COULEURS.length; couleur++) {
+        for (int couleur = 0; couleur < COULEURS_TEMPLES.length; couleur++) {
             ArrayList<Image> images = new ArrayList<>();
-            for (int cristal = 0; cristal <= LIST_COULEURS.length; cristal++) {
-                String imagePath = "/cristaux/cristal_" + LIST_COULEURS[couleur] + ".png";
+            for (int cristal = 0; cristal <= COULEURS_TEMPLES.length; cristal++) {
+                String imagePath = "/cristaux/cristal_" + COULEURS_TEMPLES[couleur] + ".png";
                 images.add(new Image(getClass().getResourceAsStream(imagePath)));
             }
             cristalImageMap.put(couleur, images);
         }
     }
 
+    /**
+     * Place les temples sur la carte en fonction de leur couleur et de leur cristal associé.
+     *
+     * @param Temples           La liste des temples à placer.
+     * @param templeImagesMap   La carte associant les images de temple à leur couleur.
+     */
     public static void placerTemples(ArrayList<Temple> Temples, TreeMap<Integer, ArrayList<Image>> templeImagesMap) {
         for (Temple t : Temples) {
             ArrayList<Image> images = templeImagesMap.get(t.getCouleur()-1);
@@ -149,6 +172,10 @@ public class CanvasCartes extends Canvas implements Constantes{
         }
     }
 
+    /**
+    * Place l'apprenti sur la carte.
+    * Si l'apprenti ne tient pas de cristal, il n'est pas affiché.
+    */
     public void placerApprenti() {
         hGC.drawImage(chImageApprenti, chApprenti.getPosition().getChAbscisse()*CARRE+2, chApprenti.getPosition().getChOrdonnee()*CARRE+2, CARRE-4, CARRE-4);
         if (chApprenti.getCouleurCristal() == 0) {
@@ -156,34 +183,48 @@ public class CanvasCartes extends Canvas implements Constantes{
         }
     }
 
+    /**
+     * Efface une case sur la carte à la position spécifiée.
+     *
+     * @param pos La position de la case à effacer.
+     */
     public void effacerCase(Position pos) {
         hGC.setFill(COULEUR_FOND);
-        //hGC.fillRect(pos.getPosX()*CARRE+2, pos.getPosY()*CARRE+2, CARRE-4, CARRE-4);
-        hGC.fillRect(pos.getPosX()*CARRE+1, pos.getPosY()*CARRE+1, CARRE-2, CARRE-2);
+        hGC.fillRect(pos.getChAbscisse()*CARRE+1, pos.getChOrdonnee()*CARRE+1, CARRE-2, CARRE-2);
     }
 
+    /**
+     * Échange les cristaux entre l'apprenti et le temple à la position spécifiée.
+     *
+     * @param posTemple La position du temple avec lequel l'échange est effectué.
+     */
     public void echangeCristals(Position posTemple) {
         Temple t = chScenario.getTemple(posTemple);
         if (t == null)
             return;
 
-        if (chApprenti.getCouleurCristal() == 0) {        // 1er arrivage, apprenti enleve le cristal
+        if (chApprenti.getCouleurCristal() == 0) {
             chApprenti.setCouleurCristal(t.getCouleurCristal());
             t.setCouleurCristal(0);
         }
-        else {              // echange les cristals
+        else {
             if (t.getCouleurCristal() != 0) {
                 int tmp = chApprenti.getCouleurCristal();
                 chApprenti.setCouleurCristal(t.getCouleurCristal());
                 t.setCouleurCristal(tmp);
             }
-            else {                  // Si coul cristal temple = 0, apprent ne change pad coul
+            else {
                 t.setCouleurCristal(chApprenti.getCouleurCristal());
             }
-
         }
     }
 
+    /**
+     * Déplace l'apprenti selon une liste de positions de temple, en utilisant une méthode spécifique.
+     *
+     * @param listeTemple La liste de positions de temple à parcourir.
+     * @param choix       La méthode de déplacement à utiliser : MANUEL, HEURISTIQUE ou SELECTION.
+     */
     public void deplApprenti(ArrayList <Position> listeTemple, int choix) {
         final int indiceListe[] = {0} ;
         final Position posTempleProchain = new Position(listeTemple.get(0));
@@ -193,45 +234,43 @@ public class CanvasCartes extends Canvas implements Constantes{
                 effacerCase(chApprenti.getPosition());
                 placerTemples(chScenario.getTemples(), templeImagesMap);
                 placerCristaux();
-                Temple temple = chScenario.getTemple(chApprenti.getPosition()) ;     // remet le temple effacé
+                Temple temple = chScenario.getTemple(chApprenti.getPosition()) ;
                 if (temple != null) {
-                    placerTemples(chScenario.getTemples(), templeImagesMap);
+                    placerTemples(chScenario.getTemples(),templeImagesMap);
                     placerCristaux();
                 }
-                chApprenti.getPosition().deplUneCase(posTempleProchain);
+                chApprenti.getPosition().deplacementUneCase(posTempleProchain);
                 placerApprenti();
 
                 Platform.runLater(() -> {
-                    //labDepl.setText("Deplacement : " + apprenti.getPosition().gerNbDepl());
                     switch (choix) {
                         case MANUEL :
-                            chVBoxInformations.setNbDeplManuel(chApprenti.getPosition().gerNbDepl());
+                            chVBoxInfo.setNbDeplacementsJouer(chApprenti.getPosition().getChNbDeplacements());
                             break;
                         case HEURISTIQUE:
-                            chVBoxInformations.setNbDeplHeuris(chApprenti.getPosition().gerNbDepl());
+                            chVBoxInfo.setNbDeplacementsHeuristique(chApprenti.getPosition().getChNbDeplacements());
                             break;
-                        case TRI:
-                            chVBoxInformations.setNbDeplTri(chApprenti.getPosition().gerNbDepl());
+                        case SELECTION:
+                            chVBoxInfo.setNbDeplacementsSelection(chApprenti.getPosition().getChNbDeplacements());
                             break;
                     }
 
                     if (chScenario.estAligne()) {
                         placerCristaux();
-                        chVBoxInformations.setInfo("VOUS AVEZ GAGNE !!!");
+                        chVBoxInfo.setInfo("VOUS AVEZ GAGNE !!!");
                         return;
                     }
                 });
 
-                if (posTempleProchain.estMemePosition(chApprenti.getPosition())) {     // arriver au temple
+                if (posTempleProchain.memePosition(chApprenti.getPosition())) {
                     echangeCristals(posTempleProchain);
-                    placerApprenti();               // pour le nouveau cristal apprenti
+                    placerApprenti();
                     indiceListe[0]++;
                     if (chScenario.estAligne() || indiceListe[0] == listeTemple.size()) {
                         if (chScenario.estAligne()) {
                             Temple last = chScenario.getTemple(chApprenti.getPosition());
                             chApprenti.setCouleurCristal(last.getCouleur());
-                            placerApprenti();   // pour cristal apprenti a la m coul que temple
-                            //System.out.println("aligne " + apprenti);
+                            placerApprenti();
                         }
                         timer.cancel();
                     }
