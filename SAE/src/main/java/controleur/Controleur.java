1ef7af5ecac30d6fa3699fe54979021f2881ea0b
sylvain922 <sylvaincouturier.pro@gmail.com>
2024-06-04 21:34:36 +0200

    ajout de la documentation

--- SAE/src/main/java/controleur/Controleur.java
+++ SAE/src/main/java/controleur/Controleur.java
@@ -6,22 +6,32 @@ import javafx.scene.control.Button;
 import vue.Constantes;
 import vue.*;
 
-
+/**
+ * Cette classe est le contrôleur principal qui gère les événements des boutons de l'interface utilisateur.
+ * Elle implémente l'interface EventHandler et utilise les constantes définies dans l'interface Constantes.
+ */
 public class Controleur implements EventHandler, Constantes {
+    /**
+     * Gère les événements déclenchés par les boutons de l'interface utilisateur.
+     * Selon le bouton cliqué, cette méthode appelle différentes actions sur le CanvasMap et met à jour l'interface utilisateur.
+     *
+     * @param event L'événement déclenché par un bouton.
+     */
     public void handle(Event event) {
-        CarteCanvas canvas = HBoxRoot.getCarteCanvas();
+        CanvasMap carte = HBoxRoot.getCarteCanvas();
+        VBoxInformations vbox = HBoxRoot.getVBoxInfo();
 
         Button bouton = (Button) event.getSource();
-        if (bouton.getUserData().equals("Reset")) {
-            System.out.println("Reset");
-            HBoxRoot.creerScenario();     // Recreation scenario indispensable, car temples sont modifiés
-            canvas.resetPlateau(HBoxRoot.getScenario());
+        if (bouton.getUserData().equals(TITRE_BOUTON_RESET)) {      //Si le bouton Reset est cliqué
+            System.out.println(TITRE_BOUTON_RESET);
+            HBoxRoot.creerScenario();
+            carte.resetPlateau(HBoxRoot.getScenario());
         }
-        else if (bouton.getUserData().equals("Tri Selection")) {
-            canvas.actionTri();
+        else if (bouton.getUserData().equals(TITRE_BOUTON_SELECTION)) {     //Si le bouton Sélection est cliqué
+            carte.actionSelection();
         }
-        else if (bouton.getUserData().equals("Tri Heuristique")) {
-            canvas.actionHeuristique();
+        else if (bouton.getUserData().equals(TITRE_BOUTON_HEURISTIQUE)) {       //Si le bouton Heuristique est cliqué
+            carte.actionHeuristique();
         }
     }
 }
